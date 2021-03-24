8647cc7e20dd00696e50f39f3d04042cc398a6f8
Mario Souto <soutomarios@gmail.com>
2021-03-24 00:44:36 -0300

    fim m6a2

--- projetos/site/pages/index.tsx
+++ projetos/site/pages/index.tsx
@@ -1,5 +1,5 @@
 import styled from 'styled-components'
-import Button from '@omariosouto/modulo-comum/components/Button'
+import Button from '@omariosouto/ui/src/components/Button'
 
 const Title = styled.h1`
   color: blue;
@@ -9,9 +9,10 @@ const Title = styled.h1`
 export default function Home() {
   return (
     <div>
-      <Button>
+      <Button />
+      {/* <Button>
         Adicionando children onde estamos usando o componente
-      </Button>
+      </Button> */}
       <Title>Nosso site que usa a lib de componentes local</Title>
     </div>
   )
